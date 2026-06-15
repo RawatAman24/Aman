d30ccd11fe0b48002d6ee718cd341a3a44f87a5a
Rawat Aman <2402031000053@silveroakuni.ac.in>
2026-06-15 18:31:00 +0530

    Added new folder and new file

--- script.js
+++ script.js
@@ -189,3 +189,19 @@ async function getWeather() {
         console.log(error);
     }
 }
+/* Add to cart*/
+function addToCart(name, price) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    let existing = cart.find(item => item.name === name);
+    if (existing) {
+        existing.quantity += 1;
+    } else {
+        cart.push({
+            name: name,
+            price: price,
+            quantity: 1
+        });
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert(name + " added to cart!");
+}
