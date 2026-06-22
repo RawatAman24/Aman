3beaea88e07cb9efbac1948ef5e9428b6b3d8734
Rawat Aman <2402031000053@silveroakuni.ac.in>
2026-06-22 20:07:36 +0530

    Eddited Old file

--- Block Stack/Block.js
+++ Block Stack/Block.js
@@ -1,17 +1,13 @@
 function generateBox(x, y, z, width, depth, falls) {
     const geometry = new THREE.BoxGeometry(width, boxHeight, depth);
     const color = new THREE.Color(
-        `hsl(${30 + stack.length * 4}, 100%, 50%)`
-    );
+        `hsl(${30 + stack.length * 4}, 100%, 50%)`);
     const material = new THREE.MeshLambertMaterial({ color });
     const mesh = new THREE.Mesh(geometry, material);
-
     mesh.position.set(x, y, z);
     scene.add(mesh);
-
     const shape = new CANNON.Box(
-        new CANNON.Vec3(width / 2, boxHeight / 2, depth / 2)
-    );
+        new CANNON.Vec3(width / 2, boxHeight / 2, depth / 2));
     let mass = falls ? 5 : 0;
     mass *= width / originalBoxSize;
     mass *= depth / originalBoxSize;
